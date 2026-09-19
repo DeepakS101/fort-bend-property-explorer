bb92c4e4342964c836736e1b06478a5f265e4c7b
DeepakS101 <168841863+DeepakS101@users.noreply.github.com>
2026-09-19 13:08:56 -0500

    make_database.py

--- make_database.py
+++ make_database.py
@@ -4,11 +4,9 @@ import sqlite3
 print("Opening the CSV file...")
 df = pd.read_csv("fort_bend_properties.csv")
 
-# Creating a brand new SQLite database file
 print("Building the SQLite database...")
 conn = sqlite3.connect("properties.db")
 
-# Pouring all the CSV data into a database table called 'house_data'
 print("Transferring data... (this might take a few seconds)")
 df.to_sql("house_data", conn, if_exists="replace", index=False)
 
